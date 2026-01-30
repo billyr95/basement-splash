92aed4bea9b9d98a09f68643cb90c71744d641a8
audstudios <billy.e.riley@gmail.com>
2026-01-30 18:46:54 -0500

    ASCII in the dev terminal

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,20 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* ASCII Art Easter Egg */}
+        {`<!--
+ BBBS   _    _       _  _     _      _     _    _  RFC
+ ______／ ＼__／ ＼  ___／ ＼／ ＼___／ ＼ ___／ ＼___／ ＼__／ ＼____
+／  _  __ ／＼  _ ＼／  ___／＼  ___＼  ˇ  ／＼  ___＼    ＼__    ＼
+＼_／／  __ ＼／  ＼  ＼___  ＼／  __／  ＼／  ＼／  __／   ／ ／ ／ ／＼_／
+   ＼__   ／＼   ＼_／  ___／___  ＼__／   ／___  ＼__／  ＼／  ＼  
+ 20   ＼_／  ＼__／ ＼_／       ＼_／  ＼__／    ＼_／  ＼__／＼__／ 26
+            Basement Bulletin Board System
+                   [WEBSITE LOADING]
+
+Welcome to the source, curious explorer.
+Looking for something? Email us: hello@basementload.ing
+-->`}
       </head>
       <body className="antialiased">
         {children}
